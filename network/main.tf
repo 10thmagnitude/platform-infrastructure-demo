ef92228a7ced0b6761eb65b23e7458696d3a8f5f
Scott Nowicki <no.wicki.scott@gmail.com>
2018-06-19 12:13:09 -0500

    added props to config mapping; added key to resource naming

--- network/main.tf
+++ network/main.tf
@@ -6,12 +6,14 @@ provider "azurerm" {
 }
 
 locals {
-  location    = "${lookup(var.config[var.workspace], "location")}"
-  environment = "${lookup(var.config[var.workspace], "environment")}"
+  key          = "${var.workspace}"
+  location     = "${lookup(var.config[var.workspace], "location")}"
+  environment  = "${lookup(var.config[var.workspace], "environment")}"
+  service_line = "${lookup(var.config[var.workspace], "service_line")}"
 }
 
 resource "azurerm_resource_group" "demo" {
-  name     = "rg-platform-demo"
+  name     = "rg-platform-demo-${local.key}"
   location = "${local.location}"
 
   tags {
@@ -20,32 +22,33 @@ resource "azurerm_resource_group" "demo" {
 }
 
 resource "azurerm_virtual_network" "demo" {
-  name                = "vnet-platform-demo"
+  name                = "vnet-platform-demo-${local.key}"
   location            = "${local.location}"
   resource_group_name = "${azurerm_resource_group.demo.name}"
   address_space       = ["10.0.0.0/16"]
 
   tags {
-    environment = "${local.environment}"
+    environment  = "${local.environment}"
+    service_line = "${local.service_line}"
   }
 }
 
 resource "azurerm_subnet" "demo_01" {
-  name                 = "sn-platform-demo-01"
+  name                 = "sn-platform-demo--${local.key}-01"
   resource_group_name  = "${azurerm_resource_group.demo.name}"
   virtual_network_name = "${azurerm_virtual_network.demo.name}"
   address_prefix       = "10.0.1.0/24"
 }
 
 resource "azurerm_subnet" "demo_02" {
-  name                 = "sn-platform-demo-02"
+  name                 = "sn-platform-demo--${local.key}-02"
   resource_group_name  = "${azurerm_resource_group.demo.name}"
   virtual_network_name = "${azurerm_virtual_network.demo.name}"
   address_prefix       = "10.0.2.0/24"
 }
 
 resource "azurerm_subnet" "demo_03" {
-  name                 = "sn-platform-demo-03"
+  name                 = "sn-platform-demo--${local.key}-03"
   resource_group_name  = "${azurerm_resource_group.demo.name}"
   virtual_network_name = "${azurerm_virtual_network.demo.name}"
   address_prefix       = "10.0.3.0/24"
--- network/variables.tf
+++ network/variables.tf
@@ -6,7 +6,7 @@ variable "client_secret" {}
 # Example:
 # location = "${lookup(var.config[var.workspace], "location")}"
 variable "workspace" {
-  default = "A1D"
+  default = "default"
 }
 
 variable "config" {
@@ -14,23 +14,31 @@ variable "config" {
 
   default = {
     default = {
-      location    = ""
-      environment = ""
+      location         = ""
+      environment      = ""
+      environment_type = ""
+      service_line     = ""
     }
 
     A1D = {
-      location    = "centralus"
-      environment = "Development"
+      location         = "centralus"
+      environment      = "Development"
+      environment_type = "dv"
+      service_line     = "glo"
     }
 
     A1Q = {
-      location    = "centralus"
-      environment = "QA"
+      location         = "centralus"
+      environment      = "QA"
+      environment_type = "qa"
+      service_line     = "glo"
     }
 
     A1P = {
-      location    = "centralus"
-      environment = "Production"
+      location         = "centralus"
+      environment      = "Production"
+      environment_type = "pr"
+      service_line     = "glo"
     }
   }
 }
